af158acfd474ea2e32cd93159d6797a2f239d021
Michelazani <michi.zani220@gmail.com>
2024-03-10 22:16:19 +0100

    inserimento components principali

---
+++ deliverboo-front-end/src/App.vue
@@ -0,0 +1,17 @@
+<script >
+import AppHeader from './components/AppHeader.vue';
+import AppFooter from './components/AppFooter.vue';
+export default{
+  components:[AppHeader,AppFooter]
+}
+</script>
+
+<template>
+  <h1>ciao</h1>
+  <AppHeader/>
+  <AppFooter/>
+</template>
+
+<style lang="scss">
+
+</style>
