f7ca1cefebae71b38df6e34001cb1ffec21c7681
raffaelepiscitelli <raffaelepiscitelli1994@gmail.com>
2024-03-19 16:08:46 +0100

    Creazione struttura AppFooter

--- deliverboo-front-end/src/components/AppFooter.vue
+++ deliverboo-front-end/src/components/AppFooter.vue
@@ -1,5 +1,52 @@
 <template >
-    footer
+    <footer>
+        <section class="upper-footer d-flex justify-content-evenly py-4">
+            <div class="d-flex flex-column">
+                <span class="fw-bold fs-5 mb-2">Scopri Deliverboo</span>
+                <a class="text-decoration-none" href="*">Investitori</a>
+                <a class="text-decoration-none" href="*">Chi siamo</a>
+                <a class="text-decoration-none" href="*">Ristoranti</a>
+                <a class="text-decoration-none" href="*">Altro</a>
+                <a class="text-decoration-none" href="*">Pressroom</a>
+                <a class="text-decoration-none" href="*">Programmazione</a>
+                <a class="text-decoration-none" href="*">Design</a>
+                <a class="text-decoration-none" href="*">Lavora con noi</a>
+                <a class="text-decoration-none" href="*">Diventa nostro partner</a>
+            </div>
+
+            <div class="d-flex flex-column">
+                <span class="fw-bold fs-5 mb-2">Note legali</span>
+                <a class="text-decoration-none" href="*">Termini & Condizioni</a>
+                <a class="text-decoration-none" href="*">Informativa sulla privacy</a>
+                <a class="text-decoration-none" href="*">Cookies</a>
+                <a class="text-decoration-none" href="*">Classificazione dei partner</a>
+                <a class="text-decoration-none" href="*">Richieste da parte delle Auotorità Pubbliche</a>
+            </div>
+
+            <div class="d-flex flex-column">
+                <span class="fw-bold fs-5 mb-2">Aiuto</span>
+                <a class="text-decoration-none" href="*">Contatti</a>
+                <a class="text-decoration-none" href="*">FAQ</a>
+                <a class="text-decoration-none" href="*">Tipi di cucina</a>
+            </div>
+
+        </section>
+        <section class="bottom-footer d-flex justify-content-around align-items-center py-4">
+
+            <div>
+                <span>&#169 Copyright 2023 - 2024</span>
+            </div>
+
+            <div class="ps-2">
+                <i class="fa-brands fa-facebook"></i>
+                <i class="fa-brands fa-instagram"></i>
+                <i class="fa-brands fa-square-x-twitter"></i>
+                <i class="fa-brands fa-youtube"></i>
+            </div>
+
+        </section>
+
+    </footer>
 </template>
 
 <script>
@@ -7,5 +54,24 @@
 </script>
 
 <style lang="scss">
+    footer {
+        border: 1px solid red;
+    }
+    .upper-footer {
+        height: 80%;
+    }
+    a {
+        text-decoration: none;
+        margin-bottom: 0.2rem;
+    }
+    .bottom-footer {
+        border: 1px solid red;
+    }
+
+    i {
+        margin-right: 0.5rem;
+    }
+
+
 
 </style>
